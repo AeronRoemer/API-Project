a94d054b729e942a38937fccbacb3bdfc9d2727b
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-06-18 12:26:50 -0600

    Flexbox for LTR/RTL flip Finalized major CSS for main screen including full-card flip when arabic script is present. Next step is modal windows.

--- README.md
+++ README.md
@@ -1,45 +1,44 @@
-# API-Project
 Random User Generator API Project - based on treehouse courses
 TODO::
-RTL on AR SCRIPT
 -SCREENSHOTS IN README
-REVIEW & COMMENT
 RECOLOR/CSS & SASS CHANGES
-PROGRESSIVE ENHANCEMENT?
 README
 
 # EMPLOYEE DIRECTORY
 Created based on project from Teamtreehouse.com.
-
-Used provided HTML and CSS in opening drafts, eventually altered to be 100% my own. 
-If you are also student and  working on this or a similar project, feel free to reach out to me with anyquestions about this code. I've made an effort to include helpful comments anddocumentation for fellow learners. 
+If you are also student and  working on this or a similar project, feel free to reach out to me with anyquestions about this code. I've made an effort to include helpful comments and documentation for fellow learners. The CSS contains significant changes to the layout, for those interested.
 
 ## Basic Structure
 
-The employee directory calls on a public API that returns random user profiles. Clicking on one of these opens a modal window with more employee information. Some of the employees use Arabic characters, these are supported with RTL text. 
+The employee directory calls on a public API that returns random user profiles. Clicking on one of these opens a modal window with more employee information. Some of the employees use Arabic scrpt, these are supported with RTL text. 
 
 ### Concepts Demonstrated
 
 The main technologies and concepts demonstrated in this project are
 
-* javaScript: Object Oriented Programming
-* Adobe Illustrator
-* SVG Graphics
-* CSS Flexbox
-* CSS Media Queries
+* javaScript
+* jQuery
+* API calls
+* CSS 
+* Localization-Friendly Design
+* !Responsive Design
 
-## Logo
-![Phrase Hunter Logo](/images/SplashImage.png)
+## CSS Techniques
 
-The 'Phrase Hunter' image is found in the repository as both a Scalable Vector Graphic and a PNG. In some cases, SVGs are smaller but in this case the complexity of the images makes the PNG a smaller sized option. 
+* Flex layout design. 
+* Animation: Header slide in created using keyframes. CSS animations are much faster to load than jQuery animations.
+* BEM (Block, Element, Modifier) methodology used for naming CSS selectors.
 
-Benefits of the SVG include:
-* resizable without pixelation
-* contains extra data like transparency and font information
+## Localization-Friendly Design
 
-Benefits of PNG the include
-* often smaller file sizes for images with complex colors
-* transparency in PNG-24 files
+The API call returns employees with both Latin charaters in their
+names and information, and Arabic characters. The code includes
+a snippet that checks to see if the first letter of an employee's 
+name is in Arabic script. If so, Arabic specific styling rules are 
+applied. Functionality could easily be expanded to Kurdish, Farsi, Syriac or
+any of the other numerous RTL scripts. 
+
+![Right to left text on card](/images/SplashImage.png)
 
 ### The typefaces used in the logo were:
 The 'Phrase' text on the logo is based on 'Sidecar' by Fenotype -> https://fenotype.com/. The 'Hunter' text is loosely based on DIN Condensed, an Adobe Font ->https://fonts.adobe.com/. The Neon effect was created in Adobe Illustrator.
@@ -59,4 +58,7 @@ When implemented, images and details will be posted about:
 ![Full Size Device Displays Correctly](/READMEimages/QueryOK.png)
 ## Test Functionality
 
-Visit https://aeronroemer.github.io/JS-Object-Oriented-Game/ to test the functionality of this project. 
+Visit https://aeronroemer.github.io/Employee-Directory-Display/ to test the functionality of this project. 
+
+### Licsence 
+MIT
--- js/scripts.js
+++ js/scripts.js
@@ -1,109 +1,97 @@
-//Variables and No-Script-Warning Cardd
+//Variables and No-Script-Warning Card
 const $gallery = $("#gallery");
 const $cardNoJs = $('#card-no-js');
 const usersAPI = 'https://randomuser.me/api/?results=12';
 $cardNoJs.hide();
-
 
 //AJAX request as outlined in the Random User API. getJSON could also work here
 $.ajax({
     url: 'https://randomuser.me/api/?results=12',
     dataType: 'json',
     success: function(data) {
-//FUNCTION to iterate through array and create cards. 
+//function to iterate through array and create cards on successful request
 function createUserCards(data){
     $.each(data, function(index, item){
         appendCard(index, item);
     })
-}
-//FUNCTION to check for arabic characters and append 'AR' class
-function isAr(item){
-    const str = item.name.first;
-    if (str.charCodeAt(0) > 1535 && str.charCodeAt(0) < 1792){
-        console.log(str)
-        $(`#${str}`).addClass('ar')
-    } 
-}
+    }
 
-//FUNCTION called in .each to generate html and append cards to the gallery DIV
-function appendCard(index, item){
-    const $card = $(`<div class="card" id="${item.name.first}">\
-        <div class="card-img-container">\
-        <img class="card-img" src="${item.picture.large}" alt="profile picture">\
-        </div>\
-        <div class="card-info-container">\
-            <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>\
-            <p class="card-text">${item.email}</p>\
-            <p class="card-text cap">${item.location.city}</p>\
-        </div>\
+    //function to check for arabic characters and append 'AR' class.
+    //called on creation of user cards and modals
+    function isAr(item){
+        const str = item.name.first;
+        if (str.charCodeAt(0) > 1535 && str.charCodeAt(0) < 1792){
+            console.log(str)
+            $(`#${str}`).addClass('ar')
+        } 
+    }
+
+    //FUNCTION called in .each to generate html and append cards to the gallery DIV
+    function appendCard(index, item){
+        const $card = $(`
+        <div class="card" id="${item.name.first}">\
+            <div class="card__img-container">\
+            <img class="card__profile-pic" src="${item.picture.large}" alt="profile picture">\
+            </div>\
+            <div class="card__employee-info">\
+                <h3 id="name" class="card__employee-info--name">${item.name.first} ${item.name.last}</h3>\
+                <p class="card__employee-info--email">${item.email}</p>\
+                <p class="card__employee-info--location">${item.location.city}</p>\
+            </div>\
+        </div>`);
+        //creates modal passing in information from clicked card
+        $card.on('click', function(e){
+            createModal(index, item)
+            });
+        //appends newly created card and click handler
+        $gallery.append($card);
+        //checks if text is arabic, if so adds an 'ar' class
+        isAr(item)
+    };
+
+    //FUNCTION called in appendCard creates modal and click handlers for this modal
+    function createModal(index, item){
+    $modal = $(`<div class="modal-container" id=${item.name.first}>
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${item.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
+                <p class="modal-text">${item.email}</p>
+                <p class="modal-text cap">${item.location.city}</p>
+                <hr>
+                <p class="modal-text">${item.phone}</p>
+                <p class="modal-text">${item.location.street.number} ${item.location.street.name} ${item.location.city} ${item.location.state} ${item.location.postcode}</p>
+                <p class="modal-text">Age: ${item.dob.age}</p>
+                </div>
+        </div>
+
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
     </div>`);
-//creates modal passing in information from clicked card
-   $card.on('click', function(e){
-    createModal(index, item)
-    });
-//appends newly created card and click handler
-  $gallery.append($card);
-  //checks if text is arabic, if so adds an 'ar' class
-  isAr(item)
-};
-
-//FUNCTION called in appendCard creates modal and click handlers for this modal
-function createModal(index, item){
-   $modal = $(`<div class="modal-container" id=${item.name.first}>
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="${item.picture.large}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
-            <p class="modal-text">${item.email}</p>
-            <p class="modal-text cap">${item.location.city}</p>
-            <hr>
-            <p class="modal-text">${item.phone}</p>
-            <p class="modal-text">${item.location.street.number} ${item.location.street.name} ${item.location.city} ${item.location.state} ${item.location.postcode}</p>
-            <p class="modal-text">Age: ${item.dob.age}</p>
-               </div>
-    </div>
-
-    // IMPORTANT: Below is only for exceeds tasks 
-    <div class="modal-btn-container">
-        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-        <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    </div>
-   </div>`);
-//click handlers to close and move forward and back;
-$('body').append($modal);
-isAr(item)
-$('.modal-close-btn').on('click', function(event){
-    $modal.hide();
-    });
-//refernces location through .each generated 'index';
-$('.modal-prev').on('click', function(event){
-        $modal.hide();
-        const loc = (index -1);
-        createModal(loc, data.results[loc]);
-        });
-$('.modal-next').on('click', function(event){
-       $modal.hide();
-       const loc = (index +1);
-       createModal(loc, data.results[loc]);
-       });
-};
-createUserCards(data.results);
-    }    
+       
+        $('body').append($modal);
+        //checks and adds arabic class before modal is appended
+        isAr(item)
+        
+         //click handlers to close and move forward and back;
+        $('.modal-close-btn').on('click', function(event){
+            $modal.hide();
+            });
+        //refernces location through .each generated 'index';
+        $('.modal-prev').on('click', function(event){
+                $modal.hide();
+                const loc = (index -1);
+                createModal(loc, data.results[loc]);
+                });
+        $('.modal-next').on('click', function(event){
+            $modal.hide();
+            const loc = (index +1);
+            createModal(loc, data.results[loc]);
+            });
+    };
+    createUserCards(data.results);
+        }    
   });
-
-/*======================
-  Search markup: 
-
-  You can use the commented out markup below as a template
-  for your search feature, but you must use JS to create and 
-  append it to `search-container` div.
-
-  IMPORTANT: Altering the arrangement of the markup and the 
-  attributes used may break the styles or functionality.
-
-  <form action="#" method="get">
-      <input type="search" id="search-input" class="search-input" placeholder="Search...">
-      <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-  </form>
-*/
